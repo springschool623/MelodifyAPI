d9ebcbe44caf336eb8930887ce88f980f3b6fd4d
springschool623 <starxuantruong@gmail.com>
2024-07-22 11:00:36 +0700

    123

--- models/userModel.js
+++ models/userModel.js
@@ -7,7 +7,7 @@ const usersFilePath = path.join(__dirname, 'users.json');
 const readUsersFromFile = () => {
   try {
     const data = fs.readFileSync(usersFilePath, 'utf8');
-    return JSON.parse(data);
+    return data ? JSON.parse(data) : [];
   } catch (err) {
     console.error('Error reading users file:', err);
     return [];
