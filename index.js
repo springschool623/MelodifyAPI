8eb2d7a98d596a22afb378bb81c08d63092545bb
springschool623 <starxuantruong@gmail.com>
2024-07-22 10:45:33 +0700

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { getUsers, addUser } = require('./models/userModel');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors()); // Thêm dòng này để bật CORS
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
