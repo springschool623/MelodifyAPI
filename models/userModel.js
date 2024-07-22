ee0d5a6379df7237a19cf5ce5d601d165ac962e0
springschool623 <starxuantruong@gmail.com>
2024-07-22 11:12:26 +0700

    update dir

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const usersFilePath = path.join(__dirname, 'users.json');
+const usersFilePath = path.join(__dirname, '..', 'users.json');
 
 const readUsersFromFile = () => {
   try {
