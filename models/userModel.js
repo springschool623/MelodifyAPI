6fb5962377cf0caca7d71dd45e0659d5a3d3d587
springschool623 <starxuantruong@gmail.com>
2024-07-22 10:49:33 +0700

    addUserUpdate

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,19 @@
 const users = [
-    { id: 1, username: 'user1', password: '123456' },
-    { id: 2, username: 'user2', password: '654321' }
-  ];
-  
-  const getUsers = () => {
-    return users;
-  };
-  
-  const addUser = (user) => {
-    users.push(user);
-  };
-  
-  module.exports = {
-    getUsers,
-    addUser
-  };
-  
+  { id: 1, username: 'user1', password: '123456' },
+  { id: 2, username: 'user2', password: '654321' }
+];
+
+const getUsers = () => {
+  return users;
+};
+
+const addUser = (user) => {
+  const maxId = users.reduce((max, user) => user.id > max ? user.id : max, 0);
+  user.id = maxId + 1;
+  users.push(user);
+};
+
+module.exports = {
+  getUsers,
+  addUser
+};
